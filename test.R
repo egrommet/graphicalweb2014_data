03eb45fda24436ae9e4d4fdfd8eddc254fba34db
mupini <ranie@ransomempini.com>
2014-08-28 00:41:38 +0100

    clean up

--- test.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Documents/mpini/github/graphicalweb2014_data")
-hist(rnorm(100))
-hist(rnorm(1000))
-x <- 8
-X <- 3
-ambulances_data <- read.csv('ambulances_queuing.csv', nrow=20, header=T)
-other_object <- names(ambulances_data)
-class(ambulances_data$Code)
-class(ambulances_data$Monday)
